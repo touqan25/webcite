2f36f1ff8a87877b5c6d3798b06f24d3db3f110a
touqan25 <143666939+touqan25@users.noreply.github.com>
2024-04-15 10:06:11 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,60 @@
-// These refer to the copy buttons in my html
-// There are two buttons, one for APA7 and one for MLA
-// A user will be able to click on these buttons to copy the citation in the
-// appropriate format
-const copyButton = document.querySelector("#copyButton");
-const copyButton2 = document.querySelector("#copyButton2");
+document.querySelector('#copyButton').addEventListener('click', function() {
+    // Message to be sent
+    const message = {
+        type: 'GREETING',
+        content: 'Hello from the popup!'
+    };
 
-// These variables are grabbing stuff from my html file, rather than
-// grabbing the current Chrome tab page's title and URL.
-// this is where I am stuck regarding that first question I had
-const webTitle = document.querySelector("head > title").textContent;
-const webURL = window.location.href;
-
-// this is the function that formats the MLA Accessed date, I tried it and it works!
-// I am not sure if this is the most efficient way to do this, but it works
-const formatMLADate = (date) => {
-    const months = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."];
-    const day = date.getDate();
-    const month = months[date.getMonth()];
-    const year = date.getFullYear();
-    return `${day} ${month} ${year}`;
-}
-
-let currentDate = new Date();
-
-
-// this is the event listener for the APA7 button
-// when a user clicks this it will copy the APA7 citation to the clipboard
-// and then change the button text to "APA7 Citation Copied!" for 2 seconds to let the user know the copy event was successful 
-// we love heuristics (visibility of system status and feedback!)
-copyButton.addEventListener("click", () => {
-    copyButton.textContent = "APA7 Citation Copied!";
-    copyButton.classList.remove("not-copied")
-    copyButton.classList.add("copied")
-    navigator.clipboard.writeText(`Last Name, First Initial. (YYYY). ${webTitle}. Cite Name. ${webURL}`);
-    setTimeout(() => {
-        copyButton.textContent = "Copy APA7 Citation";
-        copyButton.classList.remove("copied")
-        copyButton.classList.add("not-copied")
-    }, 2000);
-
+    // Send message to the active tab
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, message);
+    });
 });
 
-// this is the event listener for the MLA button
-// so same thing as above, but for MLA
-copyButton2.addEventListener("click", () => {
-    copyButton2.textContent = "MLA Citation Copied!";
-    copyButton2.classList.remove("not-copied")
-    copyButton2.classList.add("copied")
-    navigator.clipboard.writeText(`Last Name, First Name. ${webTitle} Cite Name. Day Month, Year. ${webURL} Accessed ${formatMLADate(currentDate)}`);
-    setTimeout(() => {
-        copyButton2.textContent = "Copy MLA Citation";
-        copyButton2.classList.remove("copied")
-        copyButton2.classList.add("not-copied")
-    }, 2000);
-});
 
+
+// // Event listener for APA7 button
+// document.querySelector("#copyButton").addEventListener("click", () => {
+//     console.log("Message sent to content script");
+//     // Send message to content script to copy APA7 citation
+//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//         chrome.tabs.sendMessage(tabs[0].id, { action: "copyAPA7Citation" }, function (response) {
+//             // Handle response from content script (if needed)
+//             if (response && response.success) {
+//                 // Update button text and class
+//                 const copyButton = document.querySelector("#copyButton");
+//                 copyButton.textContent = "APA7 Citation Copied!";
+//                 copyButton.classList.remove("not-copied");
+//                 copyButton.classList.add("copied");
+//                 // Reset button text and class after 2 seconds
+//                 setTimeout(() => {
+//                     copyButton.textContent = "Copy APA7 Citation";
+//                     copyButton.classList.remove("copied");
+//                     copyButton.classList.add("not-copied");
+//                 }, 2000);
+//             }
+//         });
+//     });
+// });
+
+// // Event listener for MLA button
+// document.querySelector("#copyButton2").addEventListener("click", () => {
+//     // Send message to content script to copy MLA citation
+//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//         chrome.tabs.sendMessage(tabs[0].id, { action: "copyMLACitation" }, function (response) {
+//             if (response && response.success) {
+//                 // Update button text and class
+//                 const copyButton2 = document.querySelector("#copyButton2");
+//                 copyButton2.textContent = "MLA Citation Copied!";
+//                 copyButton2.classList.remove("not-copied");
+//                 copyButton2.classList.add("copied");
+//                 // Reset button text and class after 2 seconds
+//                 setTimeout(() => {
+//                     copyButton2.textContent = "Copy MLA Citation";
+//                     copyButton2.classList.remove("copied");
+//                     copyButton2.classList.add("not-copied");
+//                 }, 2000);
+//             }
+//         });
+//     });
+// });
